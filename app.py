8f12da7e7a81d8c59a5e03514c8efc8741dffbae
Nathaniel Birne <nbirne71@gmail.com>
2021-03-29 15:44:45 -0400

    Add exception handling

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# Make it respond to !FantasyPoll instead? Using comment stream in subreddit. Also save comments to keep track of responses, prevent duplicates.
 # Set up venv
 # Set up Heroku
 # Do more testing
+
 # Change to correct subreddit
+# Make polls public
 
 import praw
 import requests
+import time
 from config import api_key, client_id, client_secret, username, password
 
 SUBREDDIT = 'test'
@@ -17,16 +21,24 @@ def main():
         password=password,
     )
 
-    # Scan all inbox messages - 
+    # Scan all inbox messages -   
         # If only mentions were scanned, this would exclude instances when 
         # the bot is mentioned in a reply to a comment or post made by the bot
-    for item in reddit.inbox.stream():
-        if item.subreddit == SUBREDDIT:
-            text = item.body
-            if f'u/{username.lower()}' in text.lower():
-                poll_choices = get_choices(text)
-                poll_url = make_poll(poll_choices)
-                item.reply(f'[Here\'s your poll!]({poll_url})')
+    stream = reddit.inbox.stream(skip_existing=True)
+    
+    while True:
+        # Try/Except deals with issues like outages, making sure bot keeps going (https://www.reddit.com/r/redditdev/comments/a8egy4/praw_how_to_make_subreddit_streams_as_robust_as/eca2nyk?utm_source=share&utm_medium=web2x&context=3)
+        try:
+            for item in stream:
+                if item.subreddit == SUBREDDIT:
+                    text = item.body
+                    if f'u/{username.lower()}' in text.lower():
+                        poll_choices = get_choices(text)
+                        poll_url = make_poll(poll_choices)
+                        item.reply(f'[Here\'s your poll!]({poll_url})')
+        except Exception as error:
+            print(error)
+        time.sleep(60)
 
 def get_choices(text):
     # Isolate just the choices, which are found after the bot is called
@@ -43,8 +55,8 @@ def make_poll(choices):
         'poll': {
             'title': 'Fantasy Baseball Poll',
             'answers': choices,
-            'priv': false,
-            'co': false
+            # 'priv': False,
+            'co': False
         }   
     }
     poll = requests.post('https://strawpoll.com/api/poll', json=data, headers={'API-KEY': api_key}).json()
