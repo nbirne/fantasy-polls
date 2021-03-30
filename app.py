d75ed2e3a4e2792f365580ee75498af5425543af
Nathaniel Birne <nbirne71@gmail.com>
2021-03-29 21:30:59 -0400

    Change bot trigger

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Make it respond to !FantasyPoll instead? Using comment stream in subreddit. Also save comments to keep track of responses, prevent duplicates.
 # Set up venv
 # Set up Heroku
 # Do more testing
@@ -12,6 +11,7 @@ import time
 from config import api_key, client_id, client_secret, username, password
 
 SUBREDDIT = 'test'
+TRIGGER = '!fantasypoll'
 
 def main():
     reddit = praw.Reddit(client_id=client_id, 
@@ -24,30 +24,33 @@ def main():
     # Scan all inbox messages -   
         # If only mentions were scanned, this would exclude instances when 
         # the bot is mentioned in a reply to a comment or post made by the bot
-    stream = reddit.inbox.stream(skip_existing=True)
+    stream = reddit.subreddit(SUBREDDIT).stream.comments()
     
     while True:
         # Try/Except deals with issues like outages, making sure bot keeps going (https://www.reddit.com/r/redditdev/comments/a8egy4/praw_how_to_make_subreddit_streams_as_robust_as/eca2nyk?utm_source=share&utm_medium=web2x&context=3)
         try:
-            for item in stream:
-                if item.subreddit == SUBREDDIT:
-                    text = item.body
-                    if f'u/{username.lower()}' in text.lower():
-                        poll_choices = get_choices(text)
-                        poll_url = make_poll(poll_choices)
-                        item.reply(f'[Here\'s your poll!]({poll_url})')
+            for comment in stream:
+                text = comment.body
+                print(text)
+                # Save comments after response, to eliminate duplication on restart (stream picks up 100 most recent)
+                if TRIGGER in text.lower() and not comment.saved:
+                    poll_choices = get_choices(text)
+                    poll_url = make_poll(poll_choices)
+                    comment.reply(f'[Here\'s your poll!]({poll_url})')
+                    comment.save()
+                    
         except Exception as error:
             print(error)
         time.sleep(60)
 
+# Isolate just the choices, which are found after the bot is called
 def get_choices(text):
-    # Isolate just the choices, which are found after the bot is called
-    word_list = text.split()
-    for i, word in enumerate(word_list):
-        if f'{username.lower()}' in word.lower():
-            word_list = word_list[i+1:]
-            break
-    poll_choices = ' '.join(word_list).split(', ')
+    start = text.lower().find(TRIGGER) + len(TRIGGER) + 1
+    stop = text.find('\n', start)
+    if stop == -1:
+        stop = None
+    text = text[start:stop]
+    poll_choices = text.split(', ')
     return poll_choices
 
 def make_poll(choices):
